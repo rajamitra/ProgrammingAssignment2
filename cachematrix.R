bf2a1f1b0115e5567c977c0dc97a7370b45cb43f
rmitra <mitra.rajeshwar@gmail.com>
2014-12-15 10:18:24 -0600

    first attempt to place this code to vesion control.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,52 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  
+  set <- function(y){
+    
+    x <<- y
+    m <<- NULL
+    
+  }
+  get <- function() x
+  
+  setinv <- function(inv) m <<- inv
+  
+  getinv<- function() m
+  
+  list( set= set, get =get ,
+        setinv = setinv,
+        getinv = getinv)
+  
 
 }
 
 
-## Write a short comment describing this function
+## This function first tries to get the inverse matrix of 
+## the matrix passed as argument and retrn it. 
+##If the inverse is not caluculated.
+##It will calculate the inverse and return it. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m<- x$getinv()
+  
+  if(!is.null(m)){
+    
+    message("getting cached data")
+    return(m);
+    
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data)
+  
+  x$setmean(m)
+  
+  m
+  
 }
